cd4bbf948a7358b5049854550d9210b511ea1dc5
JuliDeveloper <rja-97@yandex.ru>
2022-05-07 20:08:15 +0300

    Added new view controllers

---
+++ PersonsList/Storyboards/DetailsAboutPersonViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsAboutPersonViewController.swift
+//  PersonsList
+//
+//  Created by Julia Romanenko on 07.05.2022.
+//
+
+import UIKit
+
+class DetailsAboutPersonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
